6786893cb56ec7d9364362bccfae2a1b4c64a0aa
Shivam Sharma <shivampramod95925@gmail.com>
2023-02-15 16:10:11 -0600

    added about section to the TextUtils

---
+++ src/components/About.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+
+
+export default function About() {
+    const [myStyle, setMyStyle] = useState({
+        backgroundColor: 'white',
+        color: 'black'
+    });
+
+    const [btnText, setbtnText] = useState('Enable Dark Mode');
+
+    const ToggleStyle = () => {
+        if (myStyle.color === 'white') {
+            setMyStyle({
+                color: 'black',
+                backgroundColor: 'white'
+            })
+            setbtnText('Enable Dark Mode')
+        }
+        else {
+            setMyStyle({
+                backgroundColor: 'black',
+                color: 'white'
+            })
+            setbtnText('Enable Light Mode');
+
+        }
+
+    }
+
+
+    return (
+        <>
+            <div className='container  my-5' style={myStyle}>
+                <h1 className='my-3'>About us</h1>
+                <p>Welcome to Text Analyzer, a powerful tool designed to help you analyze, understand and improve your writing. At Text Analyzer, we are passionate about language and we believe that clear and effective communication is key to success in any field.</p>
+
+                <p>Our platform offers a range of features that can help you improve your writing skills, whether you are a student, a professional, or simply someone who wants to communicate more effectively. With Text Analyzer, you can analyze the readability of your text, check for grammar and spelling errors, and even get suggestions for better word choices.</p>
+
+                <p>Our team of experts is dedicated to providing you with the best possible experience. We are constantly working to improve our platform and add new features, so you can be sure that you are always getting the most up-to-date and useful tools.</p>
+
+                <p>We are committed to ensuring that our platform is accessible to everyone, which is why we offer both a free and a premium version of our software. Our free version provides basic features, while our premium version gives you access to more advanced tools and personalized support.</p>
+
+                <p>At Text Analyzer, we believe that great writing is within reach of everyone, and we are here to help you achieve your goals. Whether you are writing a report for school, a proposal for work, or simply sending an email, we are here to help you communicate more effectively. Thank you for choosing Text Analyzer.</p>
+
+            </div>
+            <div className="container">
+                <button type='button' onClick={ToggleStyle} className='btn btn-primary' style={myStyle}>{btnText}</button>
+            </div>
+        </>
+
+    )
+}
