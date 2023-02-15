6792439a182c3073ff67faa265d026d3ce45716a
Shivam Sharma <shivampramod95925@gmail.com>
2023-02-14 23:41:52 -0600

    -changed the  background color off the  navbar

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,6 +1,8 @@
 import { useState } from 'react'
+
 export default function Textform(prop) {
-    const [text, setText] = useState('Enter text here...');
+    const [text, setText] = useState('');
+    const [Space, setSpace] = useState(0);
     const HandleUpClick = () => {
         let newText = text.toUpperCase();
         setText(newText)
@@ -10,39 +12,61 @@ export default function Textform(prop) {
         setText(newText)
     }
     const HandleClearClick = () => {
-        let newText = 'Enter text here...';
-        setText(newText)
+        let newText = '';
+        setText(newText);
+    }
+    const RemoveExtraSpace = async () => {
+        try {
+            let newText = await Promise.resolve(text.trim().replace(/\s+/g, ' '));
+            setText(newText);
+
+            let spaceNum = await Promise.resolve(text.split(' ').length - 1);
+            setSpace(spaceNum);
+        } catch (error) {
+            console.error(error);
+        }
     }
     const handelOnChange = (e) => {
         setText(e.target.value)
+        let spaceNum = text.split(' ').length - 1;
+        if (spaceNum) {
+            setSpace(spaceNum);
+        }
     }
-
     return (
         <div>
             <div className="mb-3">
                 <h1 className='my-3'>{prop.heading}</h1>
                 <textarea className="form-control" id="form-control" rows="8" value={text
-                } onChange={handelOnChange}></textarea>
+                } onChange={handelOnChange} placeholder="Enter text here..."></textarea>
             </div>
             <button className="btn btn-primary m-2" onClick={HandleUpClick}>Convert to Uppercase</button>
             <button className="btn btn-primary m-2" onClick={HandleLowClick}>Convert to Lowercase</button>
+            <button className="btn btn-primary m-2" onClick={RemoveExtraSpace}>Remove Extra Spaces</button>
             <button className="btn btn-danger m-2" onClick={HandleClearClick}>Clear Textarea</button>
             <hr />
             <div className="mb-3 row text-primary container ">
                 <div className='bg-light row border py-2 rounded justify-content-center'>
                     <label htmlFor="numberOfWords" className="col-sm-3 col-md-2 col-form-label text-center px-0">Word Count :</label>
                     <div className="col-sm-2  border-end  border-secondary">
-                        <input type="text" readOnly className="form-control-plaintext border border-sccess rounded text-center bg-success text-light" id="numberOfWords" value="45" />
+                        <input type="text" readOnly className="form-control-plaintext border border-sccess rounded text-center bg-success text-light" id="numberOfWords" value={text.split(' ').length} />
                     </div>
                     <label htmlFor="numberOfSpaces" className="col-sm-3 col-md-2 col-form-label text-center px-0">Spaces :</label>
                     <div className="col-sm-2  border-end  border-secondary text-center">
-                        <input type="text" readOnly className="form-control-plaintext border border-sccess rounded text-center bg-success text-light" id="numberOfSpaces" value="45" />
+                        <input type="text" readOnly className="form-control-plaintext border border-sccess rounded text-center bg-success text-light" id="numberOfSpaces" onChange={handelOnChange} value={Space} />
                     </div>
                     <label htmlFor="numberOfCharacters" className="col-sm-3 col-md-2  col-form-label text-center px-0">Charecters :</label>
                     <div className="col-sm-2">
-                        <input type="text" readOnly className="form-control-plaintext border border-sccess rounded text-center bg-success text-light" id="numberOfCharacters" value="45" />
+                        <input type="text" readOnly className="form-control-plaintext border border-sccess rounded text-center bg-success text-light" id="numberOfCharacters" value={text.length} />
                     </div>
                 </div>
+            </div>
+            <hr />
+
+            <div className="container">
+                <h1 className='my-5'>Here Is Summary Of Your Content </h1>
+                <p className='p-2 bg-light text-success'>Reading Time : {(0.008 * text.split(' ').length).toFixed(2)} Minutes</p>
+                <p className='mb-5'>{text}</p>
             </div>
             <hr />
         </div>
