6781da33007a409f0ff01e1d88bbd750551e02a2
Shivam Sharma <shivampramod95925@gmail.com>
2023-02-15 19:30:40 -0600

    - count all the words of text provided by a user and to perform this I used some regular expressions
     userText = userText.replace(/(^\s*)|(\s*$)/gi, ""); // regular expression to remove all extra spaces of start and end of given string
    
            userText = userText.replace(/[ ]{2,}/gi, " ");// to reduce multiline space to single line
    
            userText = userText.replace(/ /, " ");// excluding all new line space
    
            userText.split(' ').length;// final word count

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,36 +3,57 @@ import { useState } from 'react'
 export default function Textform(props) {
     const [text, setText] = useState('');
     const [Space, setSpace] = useState(0);
+    const [countWord, setWordCount] = useState(0);
+
+    const handelOnChange = (e) => {
+        setText(e.target.value)
+        SpaceCount();
+        WordCount();
+    }
     const HandleUpClick = () => {
         let newText = text.toUpperCase();
         setText(newText)
+        props.showAlert('Text converted to upper case.', 'success')
     }
     const HandleLowClick = () => {
         let newText = text.toLowerCase();
         setText(newText)
+        props.showAlert('Text converted to lower case.', 'success')
     }
     const HandleClearClick = () => {
         let newText = '';
         setText(newText);
-    }
-    const RemoveExtraSpace = async () => {
-        try {
-            let newText = await Promise.resolve(text.trim().replace(/\s+/g, ' '));
-            setText(newText);
+        props.showAlert('Text area is empty now.', 'danger')
 
-            let spaceNum = await Promise.resolve(text.split(' ').length - 1);
-            setSpace(spaceNum);
-        } catch (error) {
-            console.error(error);
+    }
+    const HandleCopyText = () => {
+        if (text.split(' ').length > 0) {
+            let textValue = text;
+            navigator.clipboard.writeText(textValue);
+            props.showAlert('Text is copied to clipboard.', 'success')
+
         }
     }
-    const handelOnChange = (e) => {
-        setText(e.target.value)
+    const RemoveExtraSpace = () => {
+        let newText = text.trim().replace(/\s+/g, ' ');
+        setText(newText);
+        props.showAlert('All extra text is removed form text.', 'success')
+        SpaceCount();
+    }
+
+    const WordCount = () => {
+        let userText = text;
+        userText = userText.replace(/(^\s*)|(\s*$)/gi, ""); // regular expression to remove all extra spaces of start and end of given string
+        userText = userText.replace(/[ ]{2,}/gi, " ");// to reduce multiline space to single line
+        userText = userText.replace(/ /, " ");// excluding all new line space
+        setWordCount(userText.split(' ').length);
+    }
+
+    const SpaceCount = () => {
         let spaceNum = text.split(' ').length - 1;
-        if (spaceNum) {
-            setSpace(spaceNum);
-        }
+        setSpace(spaceNum);
     }
+
     return (
         <div>
             <div className="mb-3">
@@ -43,6 +64,7 @@ export default function Textform(props) {
             <button className="btn btn-primary m-2" onClick={HandleUpClick}>Convert to Uppercase</button>
             <button className="btn btn-primary m-2" onClick={HandleLowClick}>Convert to Lowercase</button>
             <button className="btn btn-primary m-2" onClick={RemoveExtraSpace}>Remove Extra Spaces</button>
+            <button className="btn btn-primary m-2" onClick={HandleCopyText}>Copy Text</button>
             <button className="btn btn-danger m-2" onClick={HandleClearClick}>Clear Textarea</button>
             <hr />
             <h3 className='my-2' style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>Here Is Summary Of Your Content </h3>
@@ -50,7 +72,7 @@ export default function Textform(props) {
                 <div className='bg-light row border py-2 rounded justify-content-center'>
                     <label htmlFor="numberOfWords" className="col-sm-3 col-md-2 col-form-label text-center px-0">Word Count :</label>
                     <div className="col-sm-2  border-end  border-secondary">
-                        <input type="text" readOnly className="form-control-plaintext border border-sccess rounded text-center bg-success text-light" id="numberOfWords" value={text.split(' ').length} />
+                        <input type="text" readOnly className="form-control-plaintext border border-sccess rounded text-center bg-success text-light" id="numberOfWords" value={countWord} />
                     </div>
                     <label htmlFor="numberOfSpaces" className="col-sm-3 col-md-2 col-form-label text-center px-0">Spaces :</label>
                     <div className="col-sm-2  border-end  border-secondary text-center">
@@ -68,7 +90,7 @@ export default function Textform(props) {
 
                 <h2>Text Preview</h2>
                 <p className='p-2 bg-light text-success'>Reading Time : {(0.008 * text.split(' ').length).toFixed(2)} Minutes</p>
-                <p className='mb-5' style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>{text.length > 0 ? text : 'Enter something to preview it here'}</p>
+                <p className='mb-5' style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>{text.length > 0 ? text : 'Enter something in text box to preview it here'}</p>
             </div>
             <hr />
         </div>
